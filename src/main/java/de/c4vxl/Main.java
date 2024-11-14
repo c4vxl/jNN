ca8e6fae6725951d568c719cd1f1f6fd0d6967bc
c4vxl <manage@c4vxl.de>
2024-11-14 22:20:33 +0100

    added GPT2LMModel

--- src/main/java/de/c4vxl/Main.java
+++ src/main/java/de/c4vxl/Main.java
@@ -1,13 +1,18 @@
 package de.c4vxl;
 
 import de.c4vxl.engine.data.Tensor;
-import de.c4vxl.engine.data.TensorUtils;
+import de.c4vxl.models.GPT2LMModel;
 
 public class Main {
     public static void main(String[] args) {
-        Tensor<Integer> test = Tensor.ones(3, 10, 10, 1, 1, 10, 1, 30);
+        Tensor<Float> input_ids = Tensor.of(1F, 4F).unsqueeze(0);
 
-        System.out.println(test);
-        System.out.println(TensorUtils.withoutBatchDim(test));
+        GPT2LMModel model = new GPT2LMModel(4, 2, 2, 10, 30, true);
+
+        model.load("model_params.xml");
+
+        Tensor<Float> output = model.forward(input_ids);
+
+        System.out.println(output);
     }
 }
---
+++ src/main/java/de/c4vxl/models/GPT2LMModel.java
@@ -0,0 +1,121 @@
+package de.c4vxl.models;
+
+import de.c4vxl.engine.activation.Activation;
+import de.c4vxl.engine.activation.GELU;
+import de.c4vxl.engine.data.Tensor;
+import de.c4vxl.engine.data.TensorUtils;
+import de.c4vxl.engine.module.Module;
+import de.c4vxl.engine.nn.Embedding;
+import de.c4vxl.engine.nn.Linear;
+import de.c4vxl.engine.nn.Sequence;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class GPT2LMModel extends Module {
+    public static class CausalSelfAttention extends Module {
+        private int n_head;
+
+        public Linear q_proj;
+        public Linear k_proj;
+        public Linear v_proj;
+        public Linear c_proj;
+
+        public CausalSelfAttention(int n_embd, int n_head, int block_size, boolean bias) {
+            this.n_head = n_head;
+
+            this.q_proj = new Linear(n_embd, n_embd, bias);
+            this.k_proj = new Linear(n_embd, n_embd, bias);
+            this.v_proj = new Linear(n_embd, n_embd, bias);
+            this.c_proj = new Linear(n_embd, n_embd, bias);
+        }
+
+        public <T> Tensor<T> forward(Tensor<T> x) {
+            int B = x.size(0); // batch dim
+            int T = x.size(1); // time dim
+            int C = x.size(2); // n_embd
+
+            Tensor<Integer> mask = TensorUtils.tril(Tensor.ones(T, T)).reshape(1, 1, T, T);
+
+            Tensor<T> q = this.q_proj.forward(x).reshape(B, T, this.n_head, C / this.n_head).transpose(0, 2, 1, 3);                               // b, nh, t, hs
+            Tensor<T> k = this.q_proj.forward(x).reshape(B, T, this.n_head, C / this.n_head).transpose(0, 2, 1, 3).transpose(0, 1, 3, 2);  // b, nh, hs, t
+            Tensor<T> v = this.q_proj.forward(x).reshape(B, T, this.n_head, C / this.n_head).transpose(0, 2, 1, 3);                               // b, nh, t, hs
+
+            Tensor<T> qk = q.matmul(k) // q @ k
+                    .mul(k.valueOf(1.0 / Math.sqrt(k.size(-1)))); // q @ k / sqrt(dₖ)
+
+            qk = TensorUtils.maskedFill(qk, mask, 0, qk.valueOf("-Infinity")); // apply mask
+
+            Tensor<T> qkv = Activation.Softmax(qk).matmul(v); // multiply with v; shape: b, nh, t, hs
+
+            // re-assemble all heads
+            Tensor<T> y = qkv.reshape(B, T, C); // b, seq_len, n_embd
+            y = this.c_proj.forward(y); // b, t, n_embd
+
+            return y;
+        }
+    }
+
+    public static class Block extends Module {
+        public CausalSelfAttention attn;
+        public Sequence mlp;
+
+        public Block(int n_embd, int n_head, int block_size, boolean bias) {
+            this.attn = new CausalSelfAttention(n_embd, n_head, block_size, bias);
+            this.mlp = new Sequence(
+                    new Linear(n_embd, n_embd * 4, bias),
+                    new GELU(),
+                    new Linear(4 * n_embd, n_embd, bias)
+            );
+        }
+
+        public <T> Tensor<T> forward(Tensor<T> x) {
+            x = x.add(this.attn.forward(x)); // x + attn(x)
+            x = x.add(this.mlp.forward(x));  // x + mlp(x)
+
+            return x;
+        }
+    }
+
+    private int block_size;
+
+    public Embedding wte;
+    public Embedding wpe;
+    public List<Block> heads;
+    public Linear lm_head;
+
+    public GPT2LMModel(int n_embd, int n_head, int n_layer, int block_size, int vocab_size, boolean bias) { this(n_embd, n_head, n_layer, block_size, vocab_size, bias, Tensor.defaultDataType); }
+    public GPT2LMModel(int n_embd, int n_head, int n_layer, int block_size, int vocab_size, boolean bias, Class<?> dtype) {
+        this.block_size = block_size;
+
+        this.wte = new Embedding(vocab_size, n_embd);
+        this.wpe = new Embedding(block_size, n_embd);
+        this.heads = new ArrayList<>();
+        for (int i = 0; i < n_layer; i++)
+            this.heads.add(new Block(n_embd, n_head, block_size, bias));
+        this.lm_head = new Linear(n_embd, vocab_size, false);
+    }
+
+    public <T> Tensor<T> forward(Tensor<T> input) {
+        // working with Float internally
+        Tensor<Float> idx = input.asDType(Float.class);
+
+        int T = idx.size(1); // seq length
+
+        assert T <= this.block_size: "Sequence too large!";
+
+        // embedded vectors
+        // shape: b, t, n_embd
+        Tensor<Float> x = this.wte.forward(idx)
+                .add(this.wpe.forward(Tensor.range(idx.dtype, 0, T, 1)));
+
+        for (Block block : this.heads)
+            x = block.forward(x);
+
+
+        Tensor<Float> logits = this.lm_head.forward(x); // b, t, voc_size
+
+        return logits.asDType(input.dtype);
+    }
+}
