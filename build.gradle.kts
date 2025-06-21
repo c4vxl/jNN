4f50d09e090c02948329d288f53c3a86c0894b1f
c4vxl <manage@c4vxl.de>
2025-06-21 23:10:53 +0200

    Fixed sources not showing up in maven repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,22 +3,6 @@ plugins {
     `maven-publish`
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-
-            groupId = "de.c4vxl"
-            artifactId = "jNN"
-            version = "1.0.0"
-        }
-    }
-
-    repositories {
-        maven(layout.buildDirectory.dir("repo"))
-    }
-}
-
 group = "de.c4vxl"
 version = "1.0"
 
@@ -29,4 +13,32 @@ repositories {
 dependencies {
     implementation("com.google.code.gson:gson:2.12.1")
     implementation("org.nd4j:nd4j-native-platform:1.0.0-M2.1")
+}
+
+tasks.jar {
+    exclude("de/c4vxl/Main.class")
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            groupId = "de.c4vxl"
+            artifactId = "jNN"
+            version = "1.0.0"
+
+            artifact(tasks.named("sourcesJar"))
+            artifact(tasks.named("javadocJar"))
+        }
+    }
+
+    repositories {
+        maven(layout.buildDirectory.dir("repo"))
+    }
 }
