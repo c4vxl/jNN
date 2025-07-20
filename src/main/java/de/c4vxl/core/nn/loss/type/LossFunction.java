c025a2697866cc30a1c5104b4ce3fe19faddd744
c4vxl <manage@c4vxl.de>
2025-07-20 09:01:24 +0200

    Changed input variable naming in LossFunction

--- src/main/java/de/c4vxl/core/nn/loss/type/LossFunction.java
+++ src/main/java/de/c4vxl/core/nn/loss/type/LossFunction.java
@@ -4,5 +4,5 @@ import de.c4vxl.core.nn.module.Module;
 import de.c4vxl.core.tensor.Tensor;
 
 public abstract class LossFunction extends Module {
-    public abstract <T> Tensor<T> forward(Tensor<T> input, Tensor<T> label);
+    public abstract <T> Tensor<T> forward(Tensor<T> output, Tensor<T> target);
 }
