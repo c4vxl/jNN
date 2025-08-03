b48d34e249859d8f6bf6aad1a166f5058de4f182
c4vxl <manage@c4vxl.de>
2025-08-03 20:52:58 +0200

    Implemented reductions

--- src/main/java/de/c4vxl/core/nn/loss/MSELoss.java
+++ src/main/java/de/c4vxl/core/nn/loss/MSELoss.java
@@ -8,8 +8,14 @@ import de.c4vxl.core.tensor.operation.MSEOperation;
  * This is an implementation of Mean-square-error (MSE for short)
  */
 public class MSELoss extends LossFunction {
+    public String reduction;
+
+
+    public MSELoss() { this("mean"); }
+    public MSELoss(String reduction) { this.reduction = reduction; }
+
     @Override
     public <T> Tensor<T> forward(Tensor<T> output, Tensor<T> target) {
-        return new MSEOperation<>(output, target).forward();
+        return new MSEOperation<>(output, target, this.reduction).forward();
     }
 }
--- src/main/java/de/c4vxl/core/tensor/operation/MSEOperation.java
+++ src/main/java/de/c4vxl/core/tensor/operation/MSEOperation.java
@@ -6,11 +6,16 @@ import de.c4vxl.core.tensor.operation.type.Operation;
 public class MSEOperation<T> extends Operation<T> {
     protected Tensor<T> output, target;
 
-    public MSEOperation(Tensor<T> output, Tensor<T> target) {
+    public String reduction;
+
+    public MSEOperation(Tensor<T> output, Tensor<T> target) { this(output, target, "mean"); }
+
+    public MSEOperation(Tensor<T> output, Tensor<T> target, String reduction) {
         super(output, target);
 
         this.output = this.inputs.get(0);
         this.target = this.inputs.get(1);
+        this.reduction = reduction;
     }
 
     @Override
@@ -18,8 +23,13 @@ public class MSEOperation<T> extends Operation<T> {
         // Copy out to local state
         // This is done so that reshapes applied by backpropagation (in .sub) won't propagate to the backward pass
         Tensor<T> out = this.output.clone();
+        Tensor<T> b = out.sub(this.target).pow(2.);
 
-        return out.sub(this.target).pow(2.).sum(0, true);
+        // Handle different reductions
+        if (reduction.equalsIgnoreCase("mean"))
+            return b.mean(0, true);
+
+        return b.sum(0, true);
     }
 
     @Override
